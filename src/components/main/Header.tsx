b6c40f0af0f5908050cf35edde7d32a1232cdbba
goonerholic <goonerholic@gmail.com>
2020-09-08 02:06:55 +0900

    header component

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,14 +1,27 @@
 import React, { ReactElement } from 'react';
+/**@jsx jsx*/
 import { Row, Col, Button } from 'antd';
 import { signOut } from '../../utils/gapi';
+import { jsx, css } from '@emotion/core';
 
 interface Props {}
 
 export default function Header({}: Props): ReactElement {
   return (
-    <Row>
+    <Row
+      justify="space-between"
+      align="middle"
+      css={{
+        height: 80,
+        padding: 10,
+      }}
+    >
+      <Col>
+        <h2>Photo App</h2>
+      </Col>
       <Col>
         <Button
+          type="primary"
           onClick={() => {
             signOut();
           }}
--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Button, Row } from 'antd';
+import { Button, Row, Col } from 'antd';
 import { FetchPhotoResponse } from '../../utils/gapi';
 import { FetchOptions } from './../../utils/gapi';
 import { UserInfo } from './../../modules/user';
@@ -17,5 +17,19 @@ export default function Main({
   photo,
   googleUser,
 }: Props): ReactElement {
-  return <Row></Row>;
+  return (
+    <div
+      css={{
+        maxWidth: 1200,
+        margin: 'auto',
+      }}
+    >
+      <Row>
+        <Col>Hello</Col>
+      </Row>
+      <Row>
+        <Col>It's me</Col>
+      </Row>
+    </div>
+  );
 }
